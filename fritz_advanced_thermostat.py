b331d323d5147895860c26a6a08c1522699da59b
Nils Stein <31704359+mietzen@users.noreply.github.com>
2024-09-23 05:20:09 +0200

    ruff fixes

--- fritz_advanced_thermostat.py
+++ fritz_advanced_thermostat.py
@@ -52,19 +52,19 @@ from packaging import version
 
 
 class FritzAdvancedThermostatExecutionError(Exception):
-    """Unknown error while executing"""
+    """Unknown error while executing."""
 
 
 class FritzAdvancedThermostatCompatibilityError(Exception):
-    """Fritz!BOX is not compatible with this module"""
+    """Fritz!BOX is not compatible with this module."""
 
 
 class FritzAdvancedThermostatKeyError(KeyError):
-    """Error while obtaining a key from the Fritz!BOX"""
+    """Error while obtaining a key from the Fritz!BOX."""
 
 
 class FritzAdvancedThermostatConnectionError(ConnectionError):
-    """Error while connecting to the Fritz!BOX"""
+    """Error while connecting to the Fritz!BOX."""
 
 
 FritzAdvancedThermostatError = (FritzAdvancedThermostatExecutionError, FritzAdvancedThermostatCompatibilityError, FritzAdvancedThermostatKeyError, FritzAdvancedThermostatConnectionError)
